a7ec0dad2d9adfdd27d5fc09a6b16461ffae1e67
NNish <nnish@192.168.1.8>
2025-08-02 20:11:59 +0300

    Fix buttoms contain

--- Assets/Scripts/EntryPoint.cs
+++ Assets/Scripts/EntryPoint.cs
@@ -6,25 +6,21 @@ public class EntryPoint : MonoBehaviour
     public GameManager gameManager;
     public GirlManager girlManager;
     public MakeupManager makeupManager;
-    public PlayerHand playerHand;
     public ItemActionAnimator itemActionAnimator;
     public FaceZone faceZone;
     public TabController tabController;
+    public SpongeController spongeController;
     private void Awake()
     {
         SetInstance();
-
         gameManager.Init();
-        girlManager.Init();
+        spongeController.Init();
         girlManager.Init();
         makeupManager.Init();
-        playerHand.Init();
         faceZone.Init();
         tabController.Init();
         itemActionAnimator.Init();
-
     }
-
     private void SetInstance()
     {
         if (Instance != this && Instance != null)
--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -2,13 +2,13 @@ using UnityEngine;
 using UnityEngine.UI;
 using System.Collections.Generic;
 
-public class TabController : MonoBehaviour
+public class UIcontroller : MonoBehaviour
 {
-    public static TabController Instance;
+    public static UIcontroller Instance;
     [SerializeField] private Image nonInteractableField; 
     [SerializeField] private List<Tab> tabs;
     [SerializeField] private List<Page> pages;
-    [SerializeField] private List<Button> uiButtons; // те, на которые кликают
+    [SerializeField] private List<Button> uiButtons; //те, на которые кликают
 
     // это должен быть uicontroller
     private int currentIndex = -1;
@@ -17,11 +17,11 @@ public class TabController : MonoBehaviour
     {
         for (int i = 0; i < uiButtons.Count; i++)
         {
-            int index = i; // иначе замыкание
+            int index = i; //иначе замыкание
             uiButtons[i].onClick.AddListener(() => OnTabClicked(index));
         }
 
-        OnTabClicked(0); // активируем первую вкладку по умолчанию
+        OnTabClicked(0); //активируем первую вкладку по умолчанию
     }
 
     private void OnTabClicked(int index)
@@ -35,7 +35,8 @@ public class TabController : MonoBehaviour
         }
 
         currentIndex = index;
-
+        //если переключение то сбросим спрайт 
+        MakeupManager.Instance.ResetSelectedSprite();
     }
     public void Init()
     {
